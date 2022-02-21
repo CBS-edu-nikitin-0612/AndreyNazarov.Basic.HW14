f0d7a36b48885f863439643492599f43df434a41
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-21 13:54:49 +0300

    feat: Task4

---
+++ Task4/MyListExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Task4
+{
+    internal static class MyListExtension
+    {
+        public static T[] GetArray<T>(this IEnumerable list)
+        {
+            // Как тут можно обойтись без этого? Мы не знаем сколько элементов в list, как нам создать масси нужного размера?
+            var result = new List<T>();
+            foreach (T item in list)
+            {
+                result.Add(item);
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Task2;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var array = myList.GetArray<int>();
+
+            Console.WriteLine(array[3]);
+        }
+    }
+}
