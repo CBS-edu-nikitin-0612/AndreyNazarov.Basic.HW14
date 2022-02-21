c19339760dc9cffaf15f79047fd10966041d39b9
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-21 12:28:52 +0300

    Add project files.

---
+++ Task1/MyList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+namespace Task2
+{
+    internal class MyList<T> : IEnumerable, IEnumerator
+    {
+        private T[] array;
+        private int current = -1;
+        public MyList()
+        {
+            array = new T[10];
+        }
+
+        public int Count { get; private set; } = 0;
+
+        object IEnumerator.Current => array[current];
+
+        public void Add(T item)
+        {
+            array[Count] = item;
+            Count++;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (current < Count - 1)
+            {
+                current++;
+                return true;
+            }
+            ((IEnumerator)this).Reset();
+            return false;
+        }
+
+        void IEnumerator.Reset()
+        {
+            current = -1;
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
