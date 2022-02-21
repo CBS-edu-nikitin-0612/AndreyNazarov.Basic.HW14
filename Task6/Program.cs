4c411beaac87c09caa221000558658cda5fbfb74
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-21 14:50:22 +0300

    feat: Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var collection = GetCollection(array);
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable<int> GetCollection(int[] array)
+        {
+            foreach (var item in array)
+            {
+                if (item % 2 == 0)
+                    yield return item;
+            }
+        }
+    }
+}
